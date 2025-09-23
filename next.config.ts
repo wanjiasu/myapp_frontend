105d9e90e02d211768c57b5413e87e7f8a18d443
wanjiasu <11630918+wanjiasu@user.noreply.gitee.com>
2025-09-23 12:37:52 +0800

    fix: env

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ config({ path: resolve(process.cwd(), envFile) });
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['localhost', '127.0.0.1'],
+  allowedDevOrigins: process.env.ALLOWED_DEV_ORIGINS?.split(',') || ['localhost', '127.0.0.1'],
   env: {
     // Explicitly expose environment variables to the client
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
